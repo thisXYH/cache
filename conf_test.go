2b336eab4db5f7497ac20faf2830b63697264c26
xyh <thisxyh@gmail.com>
2021-06-24 18:23:45 +0800

    temp.

--- conf_test.go
+++ conf_test.go
@@ -5,9 +5,9 @@ import (
 )
 
 var redisC = redis.NewClient(&redis.Options{
-	Addr:     "106.52.180.232:6379",
-	Password: "1234", // no password set
-	DB:       0,      // use default DB
+	Addr:     "host:port",
+	Password: "password", // no password set
+	DB:       0,          // use default DB
 })
 
 var redisCp = NewRedisCacheProvider(redisC)
