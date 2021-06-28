172e175413859881de89cb0fd33029309609bf61
xyh <thisxyh@gmail.com>
2021-06-28 15:20:39 +0800

    增加 Example.

--- key_operation_test.go
+++ key_operation_test.go
@@ -1,28 +1,33 @@
 package cache
 
 import (
-	"testing"
+	"fmt"
+	"time"
 )
 
-var cacheOp = NewOperation("go", "cache:test", 2, redisCp, NewExpireTimeFromMinute(5, 2))
+func ExampleKeyOperation() {
+	// 创建一个缓存操作对象。
+	//  指定key的组成: go:cache:test<_flag1><_flag2>
+	//  指定缓存提供器：内存缓存，切一分钟清理一次过期key
+	//  指定缓存的过期时间为，10分钟 再加上 2分钟 的随机量 上下波动
+	//
+	// 正常情况下该变量声明成全局变量，不需要重复声明。
+	cacheOp := NewOperation("go", "cache:test", 2, NewMemoryCacheProvider(1*time.Minute), NewExpireTimeFromMinute(10, 2))
 
-func TestKeyOp(t *testing.T) {
+	// 获取 key 操作对象
+	// 这个对象中包含了组装好的完整换缓存key，
+	// go:cache:test_unixTime_1，
+	// cacheOp 指定了两个flag，这边就必须传两个参数，多、少都不行。
+	key := cacheOp.Key(time.Now(), true)
 
-	value := "test valueaa"
-	keyOp := cacheOp.Key("keyOp", "haha")
-	keyOp.MustSet(value)
+	// 获取完整缓存key
+	// fmt.Println(key.Key)
 
-	var recv string
-	keyOp.MustGet(&recv)
+	key.Set("hellow word!")
 
-	if recv != value {
-		t.Error(recv)
-	}
+	var value string
+	key.Get(&value)
 
-	keyOp.MustRemove()
-
-	exist, _ := keyOp.TryGet(&recv)
-	if exist {
-		t.Error(exist)
-	}
+	fmt.Println(value)
+	// output: hellow word!
 }
