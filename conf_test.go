2c539ad6ac6a996c45f2c15a834711dfc69b9b5e
jzhi <thisxyh@gmail.com>
2021-12-10 22:36:00 +0800

    type error.

--- conf_test.go
+++ conf_test.go
@@ -8,9 +8,9 @@ import (
 
 func getNewEveryTime() *RedisCacheProvider {
 	cli := redis.NewClient(&redis.Options{
-		Addr: "redis.cache.tongbu.com:16381",
-		//Password: "CDDayDbWNJ6zW3p1weIuv9", // no password set
-		DB: 0, // use default DB
+		Addr:     "redis ip:redis port",
+		Password: "redis password", // no password set
+		DB:       0,                // use default DB
 
 		// 最大重试次数，-1 不重试
 		// 对于一些非幂等的命令，执行重试是不合理的，比如 incr
